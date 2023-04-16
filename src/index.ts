b5095f9f113cb9fdef7f8ba16b7fdd1b460b3870
Marlon Misra <marlonmisra@marlons-mbp.myfiosgateway.com>
2023-04-16 19:55:20 -0400

    experiments w prompots, llms, agents, and serpa

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
 import * as dotenv from "dotenv";
 import { OpenAI } from "langchain";
+import { initializeAgentExecutor } from "langchain/agents";
+import { SerpAPI } from "langchain/tools";
+import { Calculator } from "langchain/tools";
+
+dotenv.config();
+
+const model = new OpenAI({ temperature: 0 });
+const tools = [
+  new SerpAPI(process.env.SERPAPI_API_KEY, {
+    location: "Austin,Texas,United States",
+    hl: "en",
+    gl: "us",
+  }),
+  new Calculator(),
+];
+
+const executor = await initializeAgentExecutor(
+  tools,
+  model,
+  "zero-shot-react-description"
+);
+console.log("Loaded agent.");
+
+const input =
+  "Who is Olivia Wilde's boyfriend?" +
+  " What is his current age raised to the 0.23 power?";
+console.log(`Executing with input "${input}"...`);
+
+const result = await executor.call({ input });
+
+console.log(`Got output ${result.output}`);
+
+
+/*
+import * as dotenv from "dotenv";
+import { OpenAI } from "langchain";
 
 dotenv.config();
 
@@ -13,3 +49,4 @@ const res = await model.call(
 );
 
 console.log(res);
+*/
